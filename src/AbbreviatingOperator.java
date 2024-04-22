951933d95da61a869b0bf8d3cda730d9e54abaf5
Tefan Haetami <tefanhaetami87@gmail.com>
2024-04-22 17:05:16 +0700

    initial commit

---
+++ src/AbbreviatingOperator.java
@@ -0,0 +1,12 @@
+public class AbbreviatingOperator {
+    public static void main(String[] args) {
+        // example of abbr operator
+        int num = 0;
+        num += 10;
+        num -= 2;
+        num /= 2;
+        num *= 2;
+
+        System.out.println(num);
+    }
+}
